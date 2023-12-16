b76d04e2dec761f9e9acbb2998070de1d9869fd5
Aarnow <arnaudfgr.dev@gmail.com>
2023-12-16 17:35:50 +0100

    Initial Project Files

---
+++ Main.cs
@@ -0,0 +1,44 @@
+﻿using Life;
+using Life.UI;
+using System;
+using UnityEngine;
+using MyMenu.Entities;
+using Life.Network;
+
+namespace MyJumper
+{
+    public class Main : Plugin
+    {
+
+        public Main(IGameAPI api):base(api)
+        {
+
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+            //MyMenu
+            try
+            {
+                Section section = new Section(Section.GetSourceName(), Section.GetSourceName(), "v1.0.0", "Aarnow");
+                Action<UIPanel> action = ui => Open(section.GetPlayer(ui));
+                section.OnlyAdmin = true;
+                section.Line = new UITabLine(section.Title, action);
+                section.Insert(true);
+            }
+            catch(Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+
+            Debug.Log($"Plugin \"MyJumper\" initialisé avec succès.");
+        }
+
+        public void Open(Player player)
+        {
+            Debug.Log("open myjumper panel");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Novalife Plugin MyJunmper
+
+Ce plugin Novalife MyJumper permet...
+
+## Table des Matières
+
+- [Installation](#installation)
+- [Utilisation](#utilisation)
+- [Fonctionnalités](#fonctionnalités)
+- [Droits de Propriété Intellectuelle](#droits-de-propriété-intellectuelle)
+
+## Installation
+
+1. Téléchargez le fichier `MyJumper.dll` depuis la page des releases de ce dépôt. 
+2. Ajoutez le fichier `MyJumper.dll` dans le dossier des plugins de votre serveur Novalife.
+
+## Utilisation
+
+Le Plugin Myjumper...
+
+1. en cours..
+
+## Fonctionnalités
+
+en cours..
+
+```csharp
+```
+
+## Droits de Propriété Intellectuelle
+
+Je vous demande simplement de respecter le temps que j'ai mis dans ce plugin.  
+Merci de ne pas vous approprier le plugin, de ne pas le copier bêtement, et de ne pas faire des trucs étranges avec.
+
+Pour discuter, contactez-moi sur discord: Aarnow  
+Serveur discord: https://discord.gg/8j2suEE9Mf
